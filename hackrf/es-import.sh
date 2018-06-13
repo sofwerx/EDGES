719389a63b90fc48a9ca1c9432666ede4c36bce3
Ian Blenke <ian@blenke.com>
2018-06-13 17:28:00 -0400

    Have es-import.sh import multiple json files at once

--- hackrf/es-import.sh
+++ hackrf/es-import.sh
@@ -10,20 +10,6 @@ CREDS=${CREDS:--u ${ES_USER}:${ES_PASSWORD}}
 
 USER="${USER:-$(whoami)}"
 
-filename="$1"
-
-if [ ! -f "$filename" ]; then
-  echo Usage: $0 filename
-  exit 1
-fi
-
-#set -x
-#curl -k ${CREDS} -sL -H "Content-Type: application/json" -XDELETE ${ES_PROTO}${ES_HOST}/${ES_INDEX}/${ES_TYPE} || true
-#curl -k ${CREDS} -sL -H "Content-Type: application/json" -XDELETE ${ES_PROTO}${ES_HOST}/${ES_INDEX} || true
-#curl -k ${CREDS} -sL -H "Content-Type: application/json" -XDELETE ${ES_PROTO}${ES_HOST}/_template/${ES_INDEX}_template || true
-#
-#curl -k ${CREDS} -sL -XGET ${ES_PROTO}${ES_HOST}"/_template/${ES_INDEX}_template?pretty"
-
 #  "template": "${ES_INDEX}",
 curl -k ${CREDS} -sL -H "Content-Type: application/json" -XPUT ${ES_PROTO}${ES_HOST}/_template/${ES_INDEX}_template --data-binary @- <<EOF || true
 {
@@ -31,7 +17,6 @@ curl -k ${CREDS} -sL -H "Content-Type: application/json" -XPUT ${ES_PROTO}${ES_H
   "mappings": {
     "${ES_TYPE}": {
       "properties": {
-        "id": { "type": "keyword" },
         "frequency": { "type": "long" },
         "power": { "type": "float" },
         "geo": { "type": "geo_point" },
@@ -44,48 +29,37 @@ curl -k ${CREDS} -sL -H "Content-Type: application/json" -XPUT ${ES_PROTO}${ES_H
   }
 }
 EOF
-
+  
 #curl -k ${CREDS} -sL -XGET ${ES_PROTO}${ES_HOST}"/_template/${ES_INDEX}_template?pretty"
 
-curl -k ${CREDS} -sL -H "Content-Type: application/json" -XPUT ${ES_PROTO}${ES_HOST}/_settings* --data-binary @- <<EOF || true
-{
-  "index": {
-    "blocks": {
-      "read_only_allow_delete": "false"
-    }
-  }
-}
+for filename in $@ ; do
+
+  if [ ! -f "$filename" ]; then
+    echo Usage: $0 'filename(s)'
+    exit 1
+  fi
+
+  count=$(jq '.edges | length' $filename)
+  seq 0 $count | while read number ; do
+    echo "processing $number of $count from $filename" 1>&2
+    record="$(jq -c .edges[$number] $filename)"
+    location=$(echo "$record" | jq -c ".location")
+    lat=$(echo $location | jq ".latitude")
+    lon=$(echo $location | jq ".longitude")
+    timestamp=$(echo $location | jq ".timestamp")
+    max=$(echo "$record" | jq ".powers | length")
+    seq 0 $max | while read index ; do
+      power=$(echo "$record" | jq ".powers[$index].power")
+      frequency=$(echo "$record" | jq ".powers[$index].freq")
+
+      if [ "$power" != null -a "$frequency" != null ]; then
+        cat <<EOF
+{ "index" : { "_index" : "${ES_INDEX}", "_type" : "${ES_TYPE}", "_id": "${timestamp}_${index}_${frequency}_${power}" } }
+{ "frequency" : $frequency, "power" : $power, "geo" : { "lat" : $lat, "lon" : $lon }, "location" : $location, "created" : $timestamp }
 EOF
+      fi
 
-#set +x
-
-count=$(jq '.edges | length' $filename)
-seq 0 $count | while read number ; do
-  location=$(jq -c ".edges[$number].location" $filename)
-  lat=$(echo $location | jq ".latitude")
-  lon=$(echo $location | jq ".longitude")
-  timestamp=$(echo $location | jq ".timestamp")
-  max=$(jq ".edges[$number].powers | length" $filename)
-  seq 0 $max | while read index ; do
-    power=$(jq ".edges[$number].powers[$index].power" $filename)
-    frequency=$(jq ".edges[$number].powers[$index].freq" $filename)
-
-    if [ "$power" != null -a "$frequency" != null ]; then
-      curl -k ${CREDS} -sL -H "Content-Type: application/json" -XPOST ${ES_PROTO}${ES_HOST}/${ES_INDEX}/${ES_TYPE} --data-binary @- <<EOF
-{
-  "id": "${timestamp}_${index}_${USER}",
-  "frequency": $frequency,
-  "power": $power,
-  "geo": {
-    "lat": $lat,
-    "lon": $lon
-  },
-  "location": $location,
-  "created": $timestamp
-}
-EOF
-      echo " ${timestamp}_${index}_${USER} "
-    fi
-
+    done | curl -k ${CREDS} -sL -H "Content-Type: application/x-ndjson" -XPOST ${ES_PROTO}${ES_HOST}/${ES_INDEX}/${ES_TYPE}/_bulk --data-binary @- > /dev/null 2>&1
   done
+
 done
