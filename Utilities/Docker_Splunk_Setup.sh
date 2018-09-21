595f30854fc5f0894b1dfa3fda3dbd1fb58a1f61
Brett Waugh <brett.waugh2@gmail.com>
2018-09-21 14:45:39 -0400

    Added script for Docker Splunk container.

---
+++ Utilities/Docker_Splunk_Setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Script should setup a containzerization of Splunk 7.1.2 using Docker. 
+# Brett Waugh 13 September 2018
+
+echo Please enter what you want the admin password to be. 
+
+read new_password
+
+echo Please wait... This could take a few minutes... 
+
+docker pull splunk/splunk
+
+echo Got the pull.
+
+docker run -p 8000:8000 -d -e SPLUNK_START_ARGS="--accept-license --seed-passwd $new_password" splunk/splunk
+
+echo Setup Complete. Please visit localhost:8000 for Splunk webpage.
